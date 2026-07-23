f7318f9d00eb3309cab49f63aea6115dd3e4fd1c
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-07-23 00:59:35 -0500

    fix: React & Typescript files added.
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <main>
+            <h1>Net Worth + Budget Calculator</h1>
+            <p>React and TypeScript are working correctly.</p>
+        </main>
+    );
+}
+
+export default App;
