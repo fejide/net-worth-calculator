a6069c320da84a8736bf03639db7c3ebdacb9fa7
Joshua Brown <97815918+fejide@users.noreply.github.com>
2026-07-27 14:55:17 -0500

    feat: Display navigation bar on all pages.
    Signed-off-by: Joshua Brown <97815918+fejide@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import NetWorthPage from "./pages/NetWorthPage";
 import BudgetPage from "./pages/BudgetPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/net-worth" element={<NetWorthPage />} />
-      <Route path="/budget" element={<BudgetPage />} />
-    </Routes>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/net-worth" element={<NetWorthPage />} />
+        <Route path="/budget" element={<BudgetPage />} />
+      </Routes>
+    </>
   );
 }
 
